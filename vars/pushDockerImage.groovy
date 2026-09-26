ade535ad8b37144d64d2aa3ab6a9b616a316f144
riyankakumari <riyanka664@gmail.com>
2026-09-26 12:08:23 +0530

    Fix indentation and add braces in pushDockerImage

--- vars/pushDockerImage.groovy
+++ vars/pushDockerImage.groovy
@@ -1,7 +1,8 @@
 def call (String project,String imageTag,String dockerHubUser ){
-  withCredentials([usernamePassword(credentialsId:"dockerHubCred", usernameVariable:"dockerHubuser",passwordVariable:"dockerHubPass")])
+  withCredentials([usernamePassword(credentialsId:"dockerHubCred", usernameVariable:"dockerHubuser",passwordVariable:"dockerHubPass")]){
   sh "docker login -u ${env.dockerHubuser} -p ${env.dockerHubPass}"
 }
-  // sh "docker image tag notes-app:latest ${env.dockerHubuser}/notes-app:latest"
+  sh "docker image tag notes-app:latest ${env.dockerHubuser}/notes-app:latest"
   sh "docker push ${env.dockerHubuser}/notes-app:latest"
+  }
 }
