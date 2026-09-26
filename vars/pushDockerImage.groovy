e134d9ffcc66f6a7e02073271eee36bfbaf6afbf
riyankakumari <riyanka664@gmail.com>
2026-09-26 12:16:15 +0530

    Refactor pushDockerImage function for improved security
    Refactor Docker image push function to use password-stdin for login and update image tagging.

--- vars/pushDockerImage.groovy
+++ vars/pushDockerImage.groovy
@@ -1,8 +1,15 @@
-def call (String project,String imageTag,String dockerHubUser ){
-  withCredentials([usernamePassword(credentialsId:"dockerHubCred", usernameVariable:"dockerHubuser",passwordVariable:"dockerHubPass")]){
-  sh "docker login -u ${env.dockerHubuser} -p ${env.dockerHubPass}"
-}
-  sh "docker image tag notes-app:latest ${env.dockerHubuser}/notes-app:latest"
-  sh "docker push ${env.dockerHubuser}/notes-app:latest"
-  }
+def call(String project, String imageTag, String dockerHubUser) {
+
+    withCredentials([
+        usernamePassword(
+            credentialsId: "dockerHubCred",
+            usernameVariable: "dockerHubuser",
+            passwordVariable: "dockerHubPass"
+        )
+    ]) {
+
+        sh "echo '${dockerHubPass}' | docker login -u '${dockerHubuser}' --password-stdin"
+
+        sh "docker push ${dockerHubUser}/${project}:${imageTag}"
+    }
 }
