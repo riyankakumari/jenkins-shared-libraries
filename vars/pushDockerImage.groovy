3f3872af2072f5af4c60a412b39aaf0eee41a8f5
riyankakumari <riyanka664@gmail.com>
2026-09-26 11:53:47 +0530

    Rename parameter from imageUser to dockerHubUser

--- vars/pushDockerImage.groovy
+++ vars/pushDockerImage.groovy
@@ -1,4 +1,4 @@
-def call (String project,String imageTag,String imageUser ){
+def call (String project,String imageTag,String dockerHubUser ){
   withCredentials([usernamePassword(credentialsId:"dockerHubCred", usernameVariable:"dockerHubuser",passwordVariable:"dockerHubPass")])
   sh "docker login -u ${env.dockerHubuser} -p ${env.dockerHubPass}"
   sh "docker image tag notes-app:latest ${env.dockerHubuser}/notes-app:latest"
